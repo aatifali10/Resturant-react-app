9fea65d94b1a2d5b496314d89741bf86b30713ec
Aatif Ali Warraich <aatifwarrraich10@gmail.com>
2022-07-21 12:36:47 +0500

    all file add in this resturant app

---
+++ src/Component/Basic/menuApi.js
@@ -0,0 +1,78 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/Tea Cake  Sponge Cake 3.jpg",
+    name: "Tea Cake",
+    category: "Breakfast",
+    price: "12₹",
+    description:
+      "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 2,
+    image: "images/Hyderabad Chicken Biryani.jpg",
+    name: "Chicken Biryani",
+    category: "Biryani",
+    price: "20₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 3,
+    image: "images/Beef Biryani.jpg",
+    name: "Beef Biryani",
+    category: "Biryani",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 4,
+    image: "images/CHIKEN GOSHT.jpg",
+    name: "CHICKEN KARACHI",
+    category: "Karahi",
+    price: "50₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 5,
+    image: "images/Chicken Karachi.jpg",
+    name: "Chicken Karahi",
+    category: "Karahi",
+    price: "80₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 6,
+    image: "images/Mutton Korma.jpg",
+    name: "Mutton Korma",
+    category: "Korma",
+    price: "180₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  
+  {
+    id: 7,
+    image: "images/Chole Biryani.jpg",
+    name: "Chole Biryani ",
+    category: "Biryani",
+    price: "60₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 8,
+    image: "images/Paratha.jpg",
+    name: "Paratha Omlete",
+    category: "Breakfast",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+];
+
+export default Menu;
