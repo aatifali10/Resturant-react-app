14986c5c8b8190eed02986b91bc90b09eb7b4520
Aatif Ali Warraich <aatifwarrraich10@gmail.com>
2022-08-02 11:02:21 +0500

    Feat: Loader when loading data

--- src/Component/Basic/Resturant.js
+++ src/Component/Basic/Resturant.js
@@ -1,28 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import Menu from "./menuApi.js";
 import MenuCard from "./MenuCard";
 import Navbar from "./Navbar";
 
-const uniqueList = [
-  ...new Set(
-    Menu.map((curElem) => {
-      return curElem.category;
-    })
-  ),
-  "All",
-];
-
-console.log(uniqueList);
-
 const Resturant = () => {
   const [menuData, setMenuData] = useState(Menu);
-  const [menuList, setMenuList] = useState(uniqueList);
+  const [menuList, setMenuList] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    setMenuList([
+      ...new Set(
+        Menu.map((curElem) => {
+          return curElem.category;
+        })
+      ),
+      "All",
+    ]);
+    setLoader(false);
+  }, []);
 
   const filterItem = (category) => {
+    setLoader(true);
     if (category === "All") {
       setMenuData(Menu);
-      return;
+      return setLoader(false);
     }
 
     const updatedList = Menu.filter((curElem) => {
@@ -30,12 +33,13 @@ const Resturant = () => {
     });
 
     setMenuData(updatedList);
+    setLoader(false);
   };
 
   return (
     <>
       <Navbar filterItem={filterItem} menuList={menuList} />
-      <MenuCard menuData={menuData} />
+      {loader ? <h1>Loding....</h1> : <MenuCard menuData={menuData} />}
     </>
   );
 };
