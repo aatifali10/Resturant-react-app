7a4baec51e1d333d091c83e13dd81c95565d67fd
Aatif Ali <67818133+aatifali10@users.noreply.github.com>
2022-08-02 10:26:04 +0500

    Main (#1)
    * Add all files in this project
    
    * Feat: Add CI in build script
    
    Co-authored-by: Aatif Ali Warraich <aatifwarrraich10@gmail.com>

--- src/Component/Basic/menuApi.js
+++ src/Component/Basic/menuApi.js
@@ -8,9 +8,18 @@ const Menu = [
     description:
       "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
   },
-
   {
     id: 2,
+    image: "images/Paratha.jpg",
+    name: "Paratha Omlete",
+    category: "Breakfast",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 3,
     image: "images/Hyderabad Chicken Biryani.jpg",
     name: "Chicken Biryani",
     category: "Biryani",
@@ -19,7 +28,7 @@ const Menu = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
   },
   {
-    id: 3,
+    id: 4,
     image: "images/Beef Biryani.jpg",
     name: "Beef Biryani",
     category: "Biryani",
@@ -28,35 +37,7 @@ const Menu = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
   },
   {
-    id: 4,
-    image: "images/CHIKEN GOSHT.jpg",
-    name: "CHICKEN KARACHI",
-    category: "Karahi",
-    price: "50₹",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-  },
-  {
     id: 5,
-    image: "images/Chicken Karachi.jpg",
-    name: "Chicken Karahi",
-    category: "Karahi",
-    price: "80₹",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-  },
-  {
-    id: 6,
-    image: "images/Mutton Korma.jpg",
-    name: "Mutton Korma",
-    category: "Korma",
-    price: "180₹",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-  },
-  
-  {
-    id: 7,
     image: "images/Chole Biryani.jpg",
     name: "Chole Biryani ",
     category: "Biryani",
@@ -65,14 +46,35 @@ const Menu = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
   },
   {
-    id: 8,
-    image: "images/Paratha.jpg",
-    name: "Paratha Omlete",
-    category: "Breakfast",
-    price: "10₹",
+    id: 6,
+    image: "images/CHIKEN GOSHT.jpg",
+    name: "CHICKEN KARACHI",
+    category: "Karahi",
+    price: "50₹",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
   },
+  {
+    id: 7,
+    image: "images/Chicken Karachi.jpg",
+    name: "Chicken Karahi",
+    category: "Karahi",
+    price: "80₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 8,
+    image: "images/Mutton Korma.jpg",
+    name: "Mutton Korma",
+    category: "Korma",
+    price: "180₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  
+  
+ 
 ];
 
 export default Menu;
